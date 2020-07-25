c644d0a555bf15fa07f33e8c7eb552aee9fd4b46
lukas1013 <leonardolukas10@gmail.com>
2020-07-25 15:13:24 -0300

    add layout and convert from base 10
    adds missing change in App.js and adds conversion from decimal to binary, octal or hexadecimal

--- src/App.js
+++ src/App.js
@@ -1,5 +1,176 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from './button/';
+
+import './styles.css';
 
 export default function App() {
-	return <div>App</div>;
+	const [base1, setBase1] = useState(parseInt(localStorage.getItem('base1')) || 10);
+	const [base2, setBase2] = useState(parseInt(localStorage.getItem('base2')) || 2);
+	const [base1Value, setBase1Value] = useState('');
+	const [base2Value, setBase2Value] = useState('');
+	
+	const hexSymbols = {
+		'A': 10,
+		'B': 11,
+		'C': 12,
+		'D': 13,
+		'E': 14,
+		'F': 15
+	}
+	
+	const hexNumbers = {
+		10: 'A',
+		11: 'B',
+		12: 'C',
+		13: 'D',
+		14: 'E',
+		15: 'F'
+	}
+
+	function selectBase1(base) {
+		try {
+			localStorage.setItem('base1', base)
+		}catch(e) {
+			alert(e.message)
+		}finally{
+			setBase1(base)
+		}
+	}
+	
+	function selectBase2(base) {
+		try {
+			localStorage.setItem('base2', base)
+		}catch(e) {
+			alert(e.message)
+		}finally{
+			setBase2(base)
+		}
+	}
+	
+	function convert() {
+		let res;
+		
+		if ((!base1Value || !base1Value.length) && (!base2Value || base2Value.length)) {
+			return
+		}
+		
+		const base = base1Value ? base1 : base2;
+		const forBase = base1Value ? base2 : base1;
+		const number = base1Value ?? base2Value;
+		
+		switch (base) {
+			case 8:
+				
+				break;
+				
+			case 10:
+				res = decimalFor(forBase, number);
+				break;
+			
+			case 16:
+				
+				break;
+			
+			//2
+			default:
+			
+				break;
+		}
+		
+		if (base1Value && base1Value.length) {
+			return setBase2Value(res)
+		}
+		
+		setBase1Value(res)
+
+	}
+	
+	function decimalFor(base, numb) {
+		let rests = [], quo = numb;
+
+		do {
+			const rest = (base === 16 && quo % base > 9) ? hexNumbers[quo % base] : quo % base;
+			rests.push(rest)
+			
+			quo = parseInt(quo / base)
+			
+			if (quo < base && quo > 0) {
+				rests.push(quo)
+			}
+			
+		} while(quo >= base)
+		
+		return rests.reverse().join('');
+	}
+	
+	return (
+		<>
+			<header>
+				<h1>Numeric Base Converter</h1>
+			</header>
+			
+			<main className='container'>
+				<input id='base1' 
+					onChange={e => setBase1Value(e.target.value)} 
+					value={base1Value}
+					tabIndex='1'
+					className={base1}
+				/>
+				
+				<Button style={{ 
+					backgroundColor: base1 === 2 ? 'var(--binaryColor)' : 'transparent',
+					color: base1 === 2 ? 'white' : 'black'
+				}} disabled={base2 === 2} onClick={() => selectBase1(2)} base='binary' />
+				
+				<Button style={{ 
+					backgroundColor: base1 === 10 ? 'var(--decimalColor)' : 'transparent',
+					color: base1 === 10 ? 'white' : 'black'
+				}} disabled={base2 === 10} onClick={() => selectBase1(10)} base='decimal' />
+			
+				<Button style={{ 
+					backgroundColor: base1 === 8 ? 'var(--octalColor)' : 'transparent',
+					color: base1 === 8 ? 'white' : 'black'
+				}} disabled={base2 === 8} onClick={() => selectBase1(8)} base='octal' />
+				
+				<Button style={{ 
+					backgroundColor: base1 === 16 ? 'var(--hexColor)' : 'transparent',
+					color: base1 === 16 ? 'white' : 'black'
+				}} disabled={base2 === 16} onClick={() => selectBase1(16)} base='hexadecimal' />
+
+				<input id='base2' 
+					onChange={e => setBase2Value(e.target.value)} 
+					value={base2Value}
+					tabIndex='2'
+					className={base2}
+				/>
+				
+				<Button style={{ 
+					backgroundColor: base2 === 2 ? 'var(--binaryColor)' : 'transparent',
+					color: base2 === 2 ? 'white' : 'black'
+				}} disabled={base1 === 2} onClick={() => selectBase2(2)} base='binary' />
+				
+				<Button style={{ 
+					backgroundColor: base2 === 10 ? 'var(--decimalColor)' : 'transparent',
+					color: base2 === 10 ? 'white' : 'black'
+				}} disabled={base1 === 10} onClick={() => selectBase2(10)} base='decimal' />
+			
+				<Button style={{ 
+					backgroundColor: base2 === 8 ? 'var(--octalColor)' : 'transparent',
+					color: base2 === 8 ? 'white' : 'black'
+				}} disabled={base1 === 8} onClick={() => selectBase2(8)} base='octal' />
+				
+				<Button style={{ 
+					backgroundColor: base2 === 16 ? 'var(--hexColor)' : 'transparent',
+					color: base2 === 16 ? 'white' : 'black'
+				}} disabled={base1 === 16} onClick={() => selectBase2(16)} base='hexadecimal' />
+				
+				<button onClick={convert} id='convert'>Convert</button>
+	
+			</main>
+			
+			<footer>
+				
+			</footer>
+		</>
+	);
 }
