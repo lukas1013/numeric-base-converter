4221a155a2117758c4e5857003c1381fd888a54b
lukas1013 <leonardolukas10@gmail.com>
2020-08-05 10:40:24 -0300

    fixes and uses useReducer instead of useState

--- src/App.js
+++ src/App.js
@@ -1,16 +1,65 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import Button from './button/';
 import { binaryFor, decimalFor, octalFor, hexadecimalFor } from './helper/conversion';
 
 import './styles.css';
 
 export default function App() {
-	const [base1, setBase1] = useState(parseInt(localStorage.getItem('base1')) || 10);
-	const [base2, setBase2] = useState(parseInt(localStorage.getItem('base2')) || 2);
-	const [base1Value, setBase1Value] = useState('');
-	const [base2Value, setBase2Value] = useState('');
 	
+	function reducer(state, action) {
+		const newState = {...state}
+		
+		function getNextBase(previous) {
+			let nextBase;
+			
+			if (previous === 10) {
+				nextBase = 8
+			}else if (previous === 8) {
+				nextBase = 16
+			}else if (previous === 16) {
+				nextBase = 2
+			}else {
+				nextBase = 10
+			}
+			
+			return nextBase
+		}
+
+		switch (action.type) {
+			case 'set base1':
+				newState.base1 = action.base1
+				if (newState.base2 === newState.base1) {
+					newState.base2 = getNextBase(newState.base2)
+				}
+				break;
+			
+			case 'set base1Value':
+				newState.base1Value = action.value
+				break;
+				
+			case 'set base2Value':
+				newState.base2Value = action.value
+				break;
+			
+			//case 'set base2':
+			default:
+				newState.base2 = action.base2
+				if (newState.base1 === newState.base2) {
+					newState.base1 = getNextBase(newState.base1)
+				}
+			
+				break;
+		}
+		
+		return newState
 	}
+	
+	const [state, dispatch] = useReducer(reducer, {
+		base1: localStorage.getItem('base1') || 10,
+		base2: localStorage.getItem('base2') || 2,
+		base1Value: null,
+		base2Value: null
+	});
 	
 	function selectBase1(base) {
 		try {
@@ -18,7 +67,10 @@ export default function App() {
 		}catch(e) {
 			alert(e.message)
 		}finally{
-			setBase1(base)
+			dispatch({
+				type: 'set base1',
+				base1: base
+			})
 		}
 	}
 	
@@ -28,20 +80,25 @@ export default function App() {
 		}catch(e) {
 			alert(e.message)
 		}finally{
-			setBase2(base)
+			dispatch({
+				type: 'set base2',
+				base2: base
+			})
 		}
 	}
 	
 	function convert() {
 		let res;
-		
-		if ((!base1Value || !base1Value.length) && (!base2Value || base2Value.length)) {
+		const convertBase1 = state.base1Value, convertBase2 = state.base2Value
+
+		if (!convertBase1 && !convertBase2) {
+			//throw Error
 			return
 		}
 		
-		const base = base1Value ? base1 : base2;
-		const forBase = base1Value ? base2 : base1;
-		const number = base1Value ?? base2Value;
+		const base = convertBase1 ? state.base1 : state.base2;
+		const forBase = convertBase1 ? state.base2 : state.base1;
+		const number = convertBase1 ? state.base1Value : state.base2Value;
 		
 		switch (base) {
 			case 8:
@@ -62,11 +119,17 @@ export default function App() {
 				break;
 		}
 		
-		if (base1Value && base1Value.length) {
-			return setBase2Value(res)
+		if (convertBase1) {
+			return dispatch({
+				type: 'set base2Value',
+				value: res
+			})
 		}
 		
-		setBase1Value(res)
+		dispatch({
+			type: 'set base1Value',
+			value: res
+		})
 
 	}
 	
@@ -78,58 +141,38 @@ export default function App() {
 			
 			<main className='container'>
 				<input id='base1' 
-					onChange={e => setBase1Value(e.target.value)} 
-					value={base1Value}
+					onChange={e => dispatch({
+						type: 'set base1Value', 
+						value: e.target.value
+					})} 
+					value={state.base1Value}
 					tabIndex='1'
-					className={base1}
 				/>
 				
-				<Button style={{ 
-					backgroundColor: base1 === 2 ? 'var(--binaryColor)' : 'transparent',
-					color: base1 === 2 ? 'white' : 'black'
-				}} disabled={base2 === 2} onClick={() => selectBase1(2)} base='binary' />
+				<Button selected={state.base1 === 2} onClick={() => selectBase1(2)} base='binary' />
 				
-				<Button style={{ 
-					backgroundColor: base1 === 10 ? 'var(--decimalColor)' : 'transparent',
-					color: base1 === 10 ? 'white' : 'black'
-				}} disabled={base2 === 10} onClick={() => selectBase1(10)} base='decimal' />
+				<Button selected={state.base1 === 10} onClick={() => selectBase1(10)} base='decimal' />
 			
-				<Button style={{ 
-					backgroundColor: base1 === 8 ? 'var(--octalColor)' : 'transparent',
-					color: base1 === 8 ? 'white' : 'black'
-				}} disabled={base2 === 8} onClick={() => selectBase1(8)} base='octal' />
+				<Button selected={state.base1 === 8} onClick={() => selectBase1(8)} base='octal' />
 				
-				<Button style={{ 
-					backgroundColor: base1 === 16 ? 'var(--hexColor)' : 'transparent',
-					color: base1 === 16 ? 'white' : 'black'
-				}} disabled={base2 === 16} onClick={() => selectBase1(16)} base='hexadecimal' />
+				<Button selected={state.base1 === 16} onClick={() => selectBase1(16)} base='hexadecimal' />
 
 				<input id='base2' 
-					onChange={e => setBase2Value(e.target.value)} 
-					value={base2Value}
+					onChange={e => dispatch({
+					type: 'set base2Value',
+					value: e.target.value
+					})} 
+					value={state.base2Value}
 					tabIndex='2'
-					className={base2}
 				/>
 				
-				<Button style={{ 
-					backgroundColor: base2 === 2 ? 'var(--binaryColor)' : 'transparent',
-					color: base2 === 2 ? 'white' : 'black'
-				}} disabled={base1 === 2} onClick={() => selectBase2(2)} base='binary' />
+				<Button selected={state.base2 === 2} onClick={() => selectBase2(2)} base='binary' />
 				
-				<Button style={{ 
-					backgroundColor: base2 === 10 ? 'var(--decimalColor)' : 'transparent',
-					color: base2 === 10 ? 'white' : 'black'
-				}} disabled={base1 === 10} onClick={() => selectBase2(10)} base='decimal' />
+				<Button selected={state.base2 === 10} onClick={() => selectBase2(10)} base='decimal' />
 			
-				<Button style={{ 
-					backgroundColor: base2 === 8 ? 'var(--octalColor)' : 'transparent',
-					color: base2 === 8 ? 'white' : 'black'
-				}} disabled={base1 === 8} onClick={() => selectBase2(8)} base='octal' />
+				<Button selected={state.base2 === 8} onClick={() => selectBase2(8)} base='octal' />
 				
-				<Button style={{ 
-					backgroundColor: base2 === 16 ? 'var(--hexColor)' : 'transparent',
-					color: base2 === 16 ? 'white' : 'black'
-				}} disabled={base1 === 16} onClick={() => selectBase2(16)} base='hexadecimal' />
+				<Button selected={state.base2 === 16} onClick={() => selectBase2(16)} base='hexadecimal' />
 				
 				<button onClick={convert} id='convert'>Convert</button>
 	
--- src/button/index.js
+++ src/button/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 
 import './styles.css';
 
-export default function Button({ style, disabled, onClick, base }) {
+export default function Button({ selected, onClick, base }) {
+	const style = {
+		backgroundColor: 'transparent',
+		color: `var(--${base}Color)`,
+		boxShadow: 'none',
+		fontWeight: 'bold',
+		textShadow: 'none'
+	}
+	
 	return (
 		<button 
 			onClick={onClick}
-			disabled={disabled}
-			style={style}
+			style={selected ? style : {}}
 			className={base}>
 			{base.replace(/^[bdho]/, base.charAt(0).toUpperCase())}
 		</button>
